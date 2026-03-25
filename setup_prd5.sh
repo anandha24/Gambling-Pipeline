52744ff949b6e85f26ddb1e2a11e2e577257bbd3
anjas.muhammad <anjas.muhammad@wartek.belajar.id>
2026-03-26 03:10:27 +0700

    change command setup_prd5.sh dir path

--- setup_prd5.sh
+++ setup_prd5.sh
@@ -10,18 +10,20 @@ echo "[2] Create Supervisor config (prd5_api)"
 
 sudo tee /etc/supervisor/conf.d/prd5_api.conf > /dev/null <<'EOF'
 [program:prd5_api]
-command=/home/ubuntu/miniconda3/envs/prd5/bin/python /home/ubuntu/tim5_prd_workdir/Gambling-Pipeline/run_server.py
-directory=/home/ubuntu/tim5_prd_workdir/Gambling-Pipeline
+command=/workspace/Gambling-Pipeline/miniconda3/bin/conda run -n prd5 python run_server.py
+directory=/workspace/Gambling-Pipeline
 autostart=true
 autorestart=true
-stderr_logfile=/var/log/prd5_api.err.log
-stdout_logfile=/var/log/prd5_api.out.log
+startsecs=5
+stderr_logfile=/workspace/logs/prd5.err.log
+stdout_logfile=/workspace/logs/prd5.out.log
 EOF
 
 echo "[3] Reload Supervisor"
 sudo /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
 sudo supervisorctl reread
 sudo supervisorctl update
+sudo supervisorctl restart prd5_api
 
 echo "[4] Start prd5_api"
 sudo supervisorctl start prd5_api || sudo supervisorctl restart prd5_api
